7e85f01cee30f574056628a443f927c6f9b5e38f
Bruno Mariz <bmarizot@gmail.com>
2025-07-15 14:53:23 -0300

    fix: allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,12 +13,7 @@ app = FastAPI(title="ProComp API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:3001",
-        "https://procomp.brunomariz.dev",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
