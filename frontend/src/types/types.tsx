0e55e58980ea3ddcbd51b58c226e1399506c8683
Bruno Mariz <bmarizot@gmail.com>
2025-07-15 16:14:48 -0300

    restore frontend

---
+++ frontend/src/types/types.tsx
@@ -0,0 +1,13 @@
+export interface CompanyProfile {
+  company_name: string;
+  company_description: string;
+  tier1_keywords: string[];
+  tier2_keywords: string[];
+  emails: string[];
+  poc: string;
+}
+
+export interface WebsiteAnalysisResponse {
+  url: string;
+  analysis: CompanyProfile;
+}
