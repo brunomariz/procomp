4fd67fb9c159429c5b0dd10d7b648b4d0d968947
Bruno Mariz <bmarizot@gmail.com>
2025-07-15 14:31:55 -0300

    fix: cors config

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,13 +11,14 @@ from app.verification import verify_url
 app = FastAPI(title="ProComp API", version="1.0.0")
 
 
-origins = [
-    "*",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:3001",
+        "https://procomp.brunomariz.dev/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
